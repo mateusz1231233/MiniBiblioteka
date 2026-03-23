f4a080ecc84c51b9e4ab2c3ae0edd6879ddffb07
mateusz1231233 <s35533@pjwstk.edu.pl>
2026-03-23 23:32:06 +0100

    Dodanie klasy Ksiazka i pierwszych obiektów testowych

---
+++ src/Ksiazka.java
@@ -0,0 +1,29 @@
+public class Ksiazka {
+    private String tytul;
+    private String autor;
+    private int liczbaStron;
+    private boolean dostepna;
+    public Ksiazka(String tytul, String autor, int liczbaStron, boolean dostepna) {
+        this.tytul = tytul;
+        this.autor = autor;
+        this.liczbaStron = liczbaStron;
+        this.dostepna = dostepna;
+    }
+    public void wypiszInfo() {
+        System.out.println("Tytuł: " + tytul + ", Autor: " + autor + ", Strony: " + liczbaStron + ", Dostępna: " + dostepna);
+    }
+
+    public void wypozycz() {
+        if (dostepna == true) {
+            dostepna = false;
+            System.out.println("Wypożyczono: " + tytul);
+        } else {
+            System.out.println("Książka " + tytul + " jest już wypożyczona.");
+        }
+    }
+    public void zwroc() {
+        dostepna = true;
+        System.out.println("Zwrócono: " + tytul);
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Ksiazka ksiazka1 = new Ksiazka("Wiedźmin", "Andrzej Sapkowski", 320, true);
+        Ksiazka ksiazka2 = new Ksiazka("Hobbit", "J.R.R. Tolkien", 310, true);
+        Ksiazka ksiazka3 = new Ksiazka("Diuna", "Frank Herbert", 600, false);
+
+        ksiazka1.wypiszInfo();
+        ksiazka1.wypozycz();
+        ksiazka1.wypozycz();
+        ksiazka1.zwroc();
+        ksiazka1.wypiszInfo();
+    }
+}
